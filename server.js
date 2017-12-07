6b9d5daca3b1fad1a402adfeeb74d99a338173ac
Mukesh Kumar <M1036022@mindtree.com>
2017-12-08 02:19:27 +0530

    change

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ var mongojs = require('mongojs');
 
 
 const db = mongojs('mongodb://admin:admin@ds133166.mlab.com:33166/employeeslist', ['Employess']); 
-// var server=app.listen(process.env.port,function(req,res){
-   
-//     console.log("Server started",process.env.port)
-// })
+
+console.log("db",db)
 
 app.get('/getEmp',function(req,res){
     // res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,27 +14,32 @@ app.get('/getEmp',function(req,res){
         console.log(docs)
         res.send(docs);
     })
-
+    
 });
 app.get('/',function(req,res){
     
-        console.log("Hello")
-        res.render("Hello Heroku");
-
+    console.log("Hello")
+    res.render("Hello Heroku");
+    
 });
 
 app.post('/addEmp',function(req,res){
     // db.Employess.insert({
-    //     "id": "76-6720845",
-    //     "first_name": "Rollin",
-    //     "last_name": "Arrell",
-    //     "email": "rarrell0@linkedin.com",
-    //     "gender": "Male",
+        //     "id": "76-6720845",
+        //     "first_name": "Rollin",
+        //     "last_name": "Arrell",
+        //     "email": "rarrell0@linkedin.com",
+        //     "gender": "Male",
     //     "city": "Mwaya"
     // },function (err, docs) {
-    //     console.log(docs)
-    // })
-    console.log("Req",req)
-
-});
-
+        //     console.log(docs)
+        // })
+        console.log("Req",req)
+        
+    });
+    
+    
+    var server=app.listen(process.env.port||3001,function(req,res){
+       
+        console.log("Server started",process.env.port|3001)
+    })
