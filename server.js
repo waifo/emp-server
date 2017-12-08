5346c8ef71d154aa9b7d7b5cd492c6628830dc6f
Mukesh Kumar <M1036022@mindtree.com>
2017-12-09 01:52:58 +0530

    Header added

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', (process.env.PORT || 5000));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    next();
 });
 
 app.get('/getEmp',function(req,res){
