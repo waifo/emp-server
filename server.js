8ba339d2ad013c396b13449c247a87178a1121a8
Mukesh Kumar <M1036022@mindtree.com>
2017-12-09 04:03:15 +0530

    code cleaned

--- server.js
+++ server.js
@@ -4,11 +4,8 @@ var app=express();
 var mongojs = require('mongojs');
 var bodyParser = require('body-parser');
 
-
-
 const db = mongojs('mongodb://admin:admin@ds133166.mlab.com:33166/employeeslist', ['Employess']); 
 
-console.log("db",db)
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', (process.env.PORT || 5000));
@@ -18,37 +15,34 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.get('/',function(req,res){
+    res.send("Server for Employees");   
+});
+
 app.get('/getEmp',function(req,res){
-    //res.header("Access-Control-Allow-Origin", "*");
     db.Employess.find({},function (err, docs) {
         console.log(docs)
         res.send(docs);
     })
     
 });
-app.get('/',function(req,res){
-    
-    console.log("Hello")
-    res.send("Hello Heroku");
-    
-});
+
 
 app.post('/addEmp',function(req,res){
-    //res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     var data = {
         "id":req.body.id,
         "first_name":req.body.first_name,
         "last_name":req.body.last_name,
         "email":req.body.email,
         "gender":req.body.gender,
-        "city":req.body.city}
+        "city":req.body.city
+    }
         db.Employess.insert(data,function (err, docs) {
             console.log("1 employee added")
         })
-        // console.log("Req",data)
         res.send()
-        
     });
+
 app.post('/updateEmp',function(req,res){
     var myquery={"id":req.body.id}
     var data = {
@@ -61,7 +55,6 @@ app.post('/updateEmp',function(req,res){
         db.Employess.update(myquery, data, function(err, res){
             console.log("1 employee updated")
         })
-        // console.log("Req",data)
         res.send()
         
     });
@@ -75,6 +68,6 @@ app.post('/deleteEmp',function(req,res){
     });
     
     
-    app.listen(app.get('port'), function() {
-        console.log('Node app is running on port', app.get('port'));
-         });
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
