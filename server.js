d924ad661c1d8c9d72084fa703ffa2888cd54c7e
Mukesh Kumar <M1036022@mindtree.com>
2019-01-18 02:16:03 +0530

    cors added

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ var corsOptions = {
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors())
-app.options('*', cors(corsOptions))
+app.use(cors(corsOptions))
+app.options('*', cors())
 app.set('port', (process.env.PORT || 5000));
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
