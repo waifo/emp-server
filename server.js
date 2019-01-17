20d26967eccd6a0ac146afd29f7c676fd5a16405
Mukesh Kumar <M1036022@mindtree.com>
2019-01-17 23:49:48 +0530

    Adding chnages

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', (process.env.PORT || 5000));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin");
     next();
 });
 
